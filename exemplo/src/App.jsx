82e1b238bcc0a77dc917f0ad638d22519b5ddf53
Matheus <mattcb.rj@gmail.com>
2026-07-14 22:32:47 -0300

    aula 1 react fundamentos

---
+++ exemplo/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Matheus" />
+      <p>Este é o parágrafo do App.</p>
+      <p>Testando como ele atualiza apenas este campo</p>
+    </div>
+  );
+}
+
+export default App;
