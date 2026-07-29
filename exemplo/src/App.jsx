ee14069052aaff59f30553f98d64f6deb70d7633
Matheus <mattcb.rj@gmail.com>
2026-07-28 21:06:24 -0300

    aula 5 renderizacao condicional

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ListaFiltrada from './components/Aula04/ListaFiltrada';
 
-// Componente principal da aplicação
+import { useState } from 'react';
+import LoginStatus from './components/Aula05/LoginStatus';
+import Notification from './components/Aula05/Notification';
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showNotif, setShowNotif] = useState(false);
+
   return (
     <div>
-      <h1>Exemplo de Comunicação entre Componentes</h1>
-      <ListaFiltrada />
+      <LoginStatus isLoggedIn={isLoggedIn} />
+      <Notification showNotification={showNotif} />
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? 'Logout' : 'Login'}
+      </button>
+      <button onClick={() => setShowNotif(!showNotif)}>
+        {showNotif ? 'Ocultar' : 'Mostrar'} Notificação
+      </button>
     </div>
   );
 }
 
 export default App;
+          
