5f3a2874d1eb38acd73087090137d8c8dfe6d42d
Matheus <mattcb.rj@gmail.com>
2026-08-06 21:59:34 -0300

    aula 8 requisicoes http

--- exemplo/src/App.jsx
+++ exemplo/src/App.jsx
@@ -1,26 +1,16 @@
 
-import React from 'react';
-import ThemeCard from './components/Aula07/casa/ThemeCard';
-
-
+import Usuarios from './Usuarios';
+import Posts from './Posts';
+import ListaUsuarios from './ListaUsuarios';
 function App() {
   return (
     <div>
-      <h1>Botão com CSS Modules</h1>
-      <div style={{ display: 'flex', gap: '20px', justifyContent: 'center', marginTop: '20px' }}>
-        <ThemeCard
-        title="Tema Claro"
-        description="Este é um card com tema claro."
-        theme="light"
-      />
-      <ThemeCard
-        title="Tema Escuro"
-        description="Este é um card com tema escuro."
-        theme="dark"
-      />
-      </div>
+      <h1>JSONPlaceholder App</h1>
+      <Usuarios />
+      <Posts />
+      <ListaUsuarios />
     </div>
   );
 }
-
 export default App;
+          
