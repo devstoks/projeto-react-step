2f503d4a41da7669e5d0203fa20b133385872491
Matheus <mattcb.rj@gmail.com>
2026-08-18 20:52:36 -0300

    aula 10: react router com multi paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 
-import FeedbackForm from './components/Aula09/casa/FeedbackForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Aula10/Home';
+import About from './components/Aula10/About';
+import Contact from './components/Aula10/Contact';
+import Navbar from './components/Aula10/Navbar';
+import Navbar2 from './components/Aula10/Navbar2';
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Formulário de Feedback</h1>
-      <FeedbackForm />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Navbar2 />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
           
